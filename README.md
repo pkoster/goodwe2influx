b75ecae222150f1c6ed6f4d2be56194534d1a85c
Paul Koster <paul@kosteronline.net>
2025-04-26 21:11:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,158 @@
+# GoodWe2Influx
+
+Python 3 script to read GoodWe sensor and settings parameters and store in InfluxDB 1.x database.  
+
+## Install
+
+### Satisfy pre-requisites:
+- Python 3
+- optionally: virtual environment
+- InfluxDB 1.x database
+
+### Download goodwe2influx
+```commandline
+git clone https://github.com/pkoster/goodwe2influx.git
+```
+
+### Install dependencies
+```commandline
+pip install -r requirements.txt
+```
+GoodWe2Influx needs the following modulesS:
+- goodwe  
+  GoodWe inverter client.
+- influxdb  
+  Influx 1.x client.
+- asyncio  
+  Asynchronous IO for goodwe module.
+
+### Configure mappings  
+Mappings define the sensor and settings to be stored. 
+Use ```mapping-default.json``` as-is or make a copy and adapt it to your needs.
+```json
+{
+ "vpv1": "vpv1_V",
+ "vpv2": "vpv2_V",
+ "ipv1": "ipv1_A",
+ ...
+}
+```
+Each row adds a field with a sensor or setting value to the database. 
+
+The lefthandside string identifies the sensor or setting name of the GoodWe inverter. 
+The meaning of most names can be found in the
+[Python GoodWe library](https://github.com/marcelblijleven/goodwe/blob/58c7f8e4b0b95ce6c3ff2fbd61b4c32a6c3665c2/goodwe/dt.py).
+
+The righthandside string specifies the fieldname in the Influx database.
+
+## Run
+### Commandline arguments
+```commandline
+$ python goodwe2influx.py -h
+usage: Goodwe2Influx [-h] [--invertermac INVERTERMAC] [--inverterhost INVERTERHOST] [--interval INTERVAL] [--influxhost INFLUXHOST]
+                     [--influxport INFLUXPORT] [--influxusername INFLUXUSERNAME] [--influxpassword INFLUXPASSWORD]
+                     [--influxdatabase INFLUXDATABASE] [--influxmeasurement INFLUXMEASUREMENT] [--inverterid INVERTERID]
+                     [--mappingfile MAPPINGFILE] [-v] [--dryrun]
+
+Read Goodwe sensor and settings parameters and store in InfluxDB 1.x database.
+
+options:
+  -h, --help            show this help message and exit
+  --invertermac INVERTERMAC
+                        MAC address of Inverter. --invertermac (recommended) or --inverterhost must be specified. Leave both empty to scan
+                        for inverters with their MAC and IP addresses.
+  --inverterhost INVERTERHOST
+                        IP address, hostname or FQDN of Inverter. IP only recommended with static IP adresses. In combination with
+                        --invertermac only used for initial connection.
+  --interval INTERVAL   Time period in seconds between inverter readings. Default: 30s.
+  --influxhost INFLUXHOST
+                        Hostname or IP address of InfluxDB server. Default: localhost.
+  --influxport INFLUXPORT
+                        Network port number of InfluxDB server. Default: 8086.
+  --influxusername INFLUXUSERNAME
+                        InfluxDB username. Default: (empty).
+  --influxpassword INFLUXPASSWORD
+                        InfluxDB password. Default: (empty).
+  --influxdatabase INFLUXDATABASE
+                        InfluxDB database name (pre-created). Default: defaultdb.
+  --influxmeasurement INFLUXMEASUREMENT
+                        Influx measurement (table) name to store inverter data. Default: goodwe.
+  --inverterid INVERTERID
+                        Inverver identifier to identify inverter in database. Default: goodwe1.
+  --mappingfile MAPPINGFILE
+                        Path to file with inverter-database field mapping. Default: mapping-default.json.
+  -v, --verbose         Print startup configuration and data readings to stdout.
+  --dryrun              Do not make database changes.
+```
+### Scan inverters
+```commandline
+$ python goodwe2influx.py
+Inverter #0   IP-address: 192.168.1.89   MAC-address: 289C6E64xxxx   name: Solar-WiFi228Rxxxx
+```
+Copy the MAC-address.
+### Test reading inverter
+The ```-v``` argument prints more output.
+The ```--dryrun``` argument avoids database changes.
+```commandline
+$ python goodwe2influx.py --invertermac 289C6E64xxxx --dryrun -v                      Inverter #0   IP-address: 192.168.1.89   MAC-address: 289C6E64278E   name: Solar-WiFi228R0056                                               
+Configuration from commandline and mapping file: {
+    "invertermac": "289C6E64xxxx",
+    "inverterhost": null,
+    "interval": 30,
+    "influxhost": "localhost",
+    "influxport": 8086,
+    "influxusername": "",
+    "influxpassword": "",
+    "influxdatabase": "defaultdb",
+    "influxmeasurement": "goodwe",
+    "inverterid": "goodwe1",
+    "mappingfile": "mapping-default.json",
+    "verbose": true,
+    "dryrun": true,
+    "mappings": {
+        "vpv1": "vpv1_V",
+        "vpv2": "vpv2_V",
+        "ipv1": "ipv1_A",
+        "ipv2": "ipv2_A",
+        "ppv1": "ppv1_W",                                                                                                                           "ppv2": "ppv2_W",                                                                                                                           "ppv": "ppv_W",                                                                                                                             "vline1": "vline1_V",                                                                                                                       "vline2": "vline2_V",                                                                                                                       "vline3": "vline3_V",                                                                                                                       "vgrid1": "vgrid1_V",
+        "vgrid2": "vgrid2_V",
+        "vgrid3": "vgrid3_V",                                                                                                                       "igrid1": "igrid1_A",
+        "igrid2": "igrid2_A",                                                                                                                       "igrid3": "igrid3_A",
+        "fgrid1": "fgrid1_Hz",                                                                                                                      "fgrid2": "fgrid2_Hz",
+        "fgrid3": "fgrid3_Hz",
+        "pgrid1": "pgrid1_W",
+        "pgrid2": "pgrid2_W",
+        "pgrid3": "pgrid3_W",
+        "total_inverter_power": "total_inverter_power_W",
+        "work_mode": "work_mode",
+        "error_codes": "error_codes",
+        "warning_code": "warning_code",
+        "apparent_power": "apparent_power_W",
+        "reactive_power": "reactive_power_W",
+        "power_factor": "power_factor",
+        "temperature": "temperature_C",
+        "e_day": "e_day_kWh",
+        "e_total": "e_total_kWh",
+        "h_total": "h_total_h",
+        "safety_country": "safety_country",
+        "derating_mode": "derating_mode",
+        "grid_export": "grid_export",
+        "grid_export_limit": "grid_export_limit_pct",
+        "start": "start",
+        "stop": "stop",
+        "restart": "restart",
+        "grid_export_hw": "grid_export_hw"
+    }
+}
+Scanning for inverters.
+Update inverter IP to 192.168.1.89
+InfluxDB point: {'measurement': 'goodwe', 'tags': {'inverter': 'goodwe1'}, 'fields': {'vpv1_V': 366.6, 'vpv2_V': 383.0, 'ipv1_A': 0.4, 'ipv2_A': 0.4, 'ppv1_W': 147, 'ppv2_W': 153, 'ppv_W': 300, 'vline1_V': 393.5, 'vline2_V': 392.7, 'vline3_V': 390.7, 'vgrid1_V': 227.0, 'vgrid2_V': 227.1, 'vgrid3_V': 225.5, 'igrid1_A': 0.7, 'igrid2_A': 0.7, 'igrid3_A': 0.7, 'fgrid1_Hz': 49.99, 'fgrid2_Hz': 49.98, 'fgrid3_Hz': 49.98, 'pgrid1_W': 159, 'pgrid2_W': 159, 'pgrid3_W': 158, 'total_inverter_power_W': 236, 'work_mode': 1, 'error_codes': 0, 'warning_code': 0, 'apparent_power_W': 0, 'reactive_power_W': -124, 'power_factor': 0.901, 'temperature_C': 44.6, 'e_day_kWh': 46.9, 'e_total_kWh': 21424.7, 'h_total_h': 10549, 'safety_country': 32, 'derating_mode': 4, 'grid_export': 0, 'grid_export_limit_pct': 200, 'start': 0, 'stop': 0, 'restart': 0, 'grid_export_hw': 0}}
+```
+### Run
+Specify commandline arguments for Influx and other.
+```commandline
+$ python goodwe2influx.py --invertermac 289C6E64xxxx
+```
+## Acknowledgements
+Goodwe2influx relies on the Python [goodwe module](https://github.com/marcelblijleven/goodwe).   
+[solar2influx](https://github.com/jaccolo/solar2influx) served as inspiration.
---
+++ goodwe2influx.py
@@ -0,0 +1,250 @@
+import argparse
+import datetime
+import json
+import socket
+import sys
+import time
+import asyncio
+import goodwe
+import influxdb
+
+
+class Goodwe2Influx:
+    """
+    Read GoodWe sensor and settings parameters and store in InfluxDB 1.x database.
+    """
+    def __init__(self, inverterhostaddress: str, invertermacaddress: str, interval: int, influxhost: str,
+                 influxport: int, influxusername: str, influxpassword: str, influxdatabase: str,
+                 influxmeasurement: str, inverterid: str, mappings: dict, verbose: bool, dryrun: bool
+    ):
+        self._inverterhostaddress = inverterhostaddress
+        self._invertermacaddress = invertermacaddress
+        self._interval = interval
+        self._influxhost = influxhost
+        self._influxport = influxport
+        self._influxusername = influxusername
+        self._influxpassword = influxpassword
+        self._influxdatabase = influxdatabase
+        self._influxmeasurement = influxmeasurement
+        self._inverterid = inverterid
+        self._mappings = mappings
+        self._verbose = verbose
+        self._dryrun = dryrun
+        self._inverteripreachable = None
+
+    def run(self) -> None:
+        """
+        Read from inverter and store in database in loop.
+        """
+        while not self._update_inverter_ip_address():
+            print(f"No IP address known. Retry in {self._interval}s.", file=sys.stderr)
+            time.sleep(self._interval)
+        while True:
+            self._run()
+            time.sleep(self._interval)
+
+
+    def _run(self) -> None:
+        """
+        Read from inverter and store in database.
+        """
+        try:
+            inverterdata = self._get_inverter_data()
+            point = self._format_influxpoint(inverterdata)
+            self._write_influx(point)
+        except goodwe.RequestFailedException as e:
+            if self._verbose:
+                print(f"RequestFailedException: {self._inverterhostaddress}: {e}", file=sys.stdout)
+        except goodwe.InverterError as e:
+            if self._verbose:
+                print(f"InverterError: {self._inverterhostaddress}: {e}", file=sys.stdout)
+
+    def _get_inverter_data(self) -> dict:
+        """
+        Get sensor and settings data from Goodwe inverter.
+        Precondition: self._inverterhostaddress != None
+        :return: sensor and setting data
+        :raises: RequestFailedException, InverterError
+        """
+        async def get_inverter_data_async(host) -> dict:
+            inverter = await goodwe.connect(host)
+            sensor_data = await inverter.read_runtime_data()
+            settings_data = await inverter.read_settings_data()
+            return sensor_data | settings_data
+
+        self._update_inverter_ip_address()
+        inverterdata = asyncio.run(get_inverter_data_async(self._inverterhostaddress))
+        self._update_inverter_ip_reacheable()
+        return inverterdata
+
+
+    def _update_inverter_ip_address(self) -> bool:
+        """
+        Update IP address for inverter MAC address.
+        :returns: True if (old) host or IP address is known, False otherwise
+        """
+        if not self._invertermacaddress:
+            return True
+        if (self._inverteripreachable and (
+                datetime.datetime.now() - self._inverteripreachable) < datetime.timedelta(minutes = 15)
+        ):
+            return True
+
+        if self._verbose:
+            print(f"Scanning for inverters.", file=sys.stdout)
+        inverters = Goodwe2Influx.scan()
+        for inverter in inverters:
+            if not inverter['mac'] == self._invertermacaddress:
+                continue
+            if not self._inverterhostaddress == inverter['ip']:
+                print(f"Update inverter IP to {inverter['ip']}", file=sys.stdout)
+            self._inverterhostaddress = inverter['ip']
+            self._update_inverter_ip_reacheable()
+            return True
+        return self._inverterhostaddress is not None
+
+
+    def _update_inverter_ip_reacheable(self) -> None:
+        """
+        Update last time inverter IP was reachable.
+        """
+        self._inverteripreachable = datetime.datetime.now()
+
+
+    def _format_influxpoint(self, inverterdata: dict) -> dict:
+        """
+        Prepare Influx point from inverterdata using mappings.
+        """
+        fields = {self._mappings[mapping]: inverterdata.get(mapping) for mapping in self._mappings}
+        point = {
+            "measurement": self._influxmeasurement,
+            "tags": {
+                "inverter": self._inverterid
+            },
+            "fields": fields
+        }
+        return point
+
+
+    def _write_influx(self, point: dict) -> None:
+        """
+        Write Goodwe sensor and settings data to Influx database. Supports Influx 1.x.
+        """
+        if not self._dryrun:
+            influxdbclient = influxdb.InfluxDBClient(self._influxhost, self._influxport,
+                                                     self._influxusername, self._influxpassword,
+                                                     self._influxdatabase)
+            influxdbclient.write_points([point])
+        if self._verbose:
+            print(f"InfluxDB point: {point}", file=sys.stdout)
+
+
+    @staticmethod
+    def scan() -> list[dict]:
+        """
+        Scans network for Goodwe inverter. Replaces non-functional goodwe.search_inverters().
+        :return: list of inverters, dict per inverter with keys 'ip', 'mac' and 'name'
+        """
+        broadcast_ip_address = '255.255.255.255'
+        goodwe_port = 48899
+        goodwe_hello = "WIFIKIT-214028-READ"
+        inverters = []
+        attributes = ['ip', 'mac', 'name']
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.settimeout(0.2)
+        sock.sendto(goodwe_hello.encode('utf-8'), (broadcast_ip_address, goodwe_port))
+        while True:
+            try:
+                data, _ = sock.recvfrom(1024)
+                fields = data.decode("utf-8").split(",")
+                inverters.append({attribute: fields[i] for i, attribute in enumerate(attributes)})
+            except TimeoutError:
+                break
+        sock.close()
+        return inverters
+
+
+def load_mappings(mappingfile: str) -> dict:
+    """
+    Load inverter-database field mapping from json file.
+    """
+    with open(mappingfile, 'r') as fin:
+        mappings = json.loads(fin.read())
+        if not isinstance(mappings, dict):
+            raise TypeError("mapping file format error")
+    return mappings
+
+
+def print_inverters(inverters: list[dict]) -> None:
+    """
+    Print inverters information.
+    """
+    if not inverters:
+        print(f'No inverters found.')
+        return
+    for i, inverter in enumerate(inverters):
+        print(f"Inverter #{i}   IP-address: {inverter['ip']}   "
+              f"MAC-address: {inverter['mac']}   name: {inverter['name']}")
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog='Goodwe2Influx',
+        description='Read Goodwe sensor and settings parameters and store in InfluxDB 1.x database.')
+    parser.add_argument('--invertermac', default=None,
+                        help='MAC address of Inverter.\n'
+                             '--invertermac (recommended) or --inverterhost must be specified.\n'
+                             'Leave both empty to scan for inverters with their MAC and IP addresses.')
+    parser.add_argument('--inverterhost', default=None,
+                        help='IP address, hostname or FQDN of Inverter.\n'
+                             'IP only recommended with static IP adresses.\n'
+                             'In combination with --invertermac only used for initial connection.')
+    parser.add_argument('--interval', default=30, type=int,
+                        help='Time period in seconds between inverter readings. Default: 30s.')
+    parser.add_argument('--influxhost', default='localhost',
+                        help='Hostname or IP address of InfluxDB server. Default: localhost.')
+    parser.add_argument('--influxport', default=8086,
+                        help='Network port number of InfluxDB server. Default: 8086.')
+    parser.add_argument('--influxusername', default='',
+                        help='InfluxDB username. Default: (empty).')
+    parser.add_argument('--influxpassword', default='',
+                        help='InfluxDB password. Default: (empty).')
+    parser.add_argument('--influxdatabase', default='defaultdb',
+                        help='InfluxDB database name (pre-created). Default: defaultdb.')
+    parser.add_argument('--influxmeasurement', default='goodwe',
+                        help='Influx measurement (table) name to store inverter data. Default: goodwe.')
+    parser.add_argument('--inverterid', default='goodwe1',
+                        help='Inverver identifier to identify inverter in database. Default: goodwe1.')
+    parser.add_argument('--mappingfile', default='mapping-default.json',
+                        help='Path to file with inverter-database field mapping. Default: mapping-default.json.')
+    parser.add_argument('-v', '--verbose',
+                        action='store_true', help='Print startup configuration and data readings to stdout.')
+    parser.add_argument('--dryrun',
+                        action='store_true', help='Do not make database changes.')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    mappings = load_mappings(args.mappingfile)
+
+    if args.verbose or (not args.invertermac and not args.inverterhost):
+        print_inverters(Goodwe2Influx.scan())
+    if not args.invertermac and not args.inverterhost:
+        exit(0)
+
+    if args.verbose:
+        configjsonstr = json.dumps(vars(args)|{'mappings': mappings}, indent=4)
+        print(f'\nConfiguration from commandline and mapping file: {configjsonstr}')
+
+    gi = Goodwe2Influx(args.inverterhost, args.invertermac, args.interval, args.influxhost, args.influxport,
+                       args.influxusername, args.influxpassword, args.influxdatabase,
+                       args.influxmeasurement, args.inverterid, mappings, args.verbose, args.dryrun)
+    gi.run()
+
+
+if __name__ == "__main__":
+    main()
+
